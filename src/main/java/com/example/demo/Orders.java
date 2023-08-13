4968556747242c33d540ad519bbe545e1382a571
Kentboyd <kmboyd18@gmail.com>
2023-08-13 12:51:07 -0500

    s

--- src/main/java/com/example/demo/Orders.java
+++ src/main/java/com/example/demo/Orders.java
@@ -4,11 +4,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
-
-@Entity
+@Data
+@Entity(name = "Orders")
+@Table(name = "Orders")
 public class Orders {
     
     @Id
-    @GeneratedValue()
+    @GeneratedValue
+
+    private long id;
+
+    private String shipping_location;
+
+    private Float purchase_total;
 }
